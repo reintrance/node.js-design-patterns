91fecb4f3bfebfbc5e63f6c572004aa973af907a
reintrance <reintrance@gmail.com>
2016-12-08 21:43:25 +0300

    Simple require implementation

--- patterns/module/simple-module-loader.js
+++ patterns/module/simple-module-loader.js
@@ -7,3 +7,34 @@ function loadModule (filename, module, require) {
 
 	eval(wrapSrc);
 }
+
+var require = function (moduleName) {
+    console.log('Require invoked for module: ' + moduleName);
+
+    var id = require.resolve(moduleName);
+
+    if (require.cache[id]) {
+        return require.cache[id].exports;
+    }
+
+    // module metadata
+    var module = {
+        exports: {},
+        id: id
+    };
+
+    // update require cache
+    require.cache[id] = module;
+
+    // load the module
+    loadModuel(id, module, require);
+
+    // return exported members of the module
+    return module.exports;
+}
+
+require.cache = {};
+
+require.resolve = function (moduleName) {
+    // full module id from the moduleName
+}
