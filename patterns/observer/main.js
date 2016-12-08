86a151401587d01468d2a0f1ce3677600124d884
reintrance <reintrance@gmail.com>
2016-12-08 23:10:55 +0300

    First try of EventEmitter

---
+++ patterns/observer/main.js
@@ -0,0 +1,22 @@
+var EventEmitter = require('events').EventEmitter;
+var fs = require('fs');
+
+function findPattern(file, regex) {
+    var emitter = new EventEmitter();
+    files.forEach(function (file) {
+        fs.readFile(file, 'utf8', function (err, content) {
+            if (err) {
+                return emitter.emit('error', err);
+            }
+
+            emitter.emit('fileread', file);
+            var match = null;
+
+            if (match = content.match(regex)) {
+                match.forEach(function (elem) {
+                    emitter.emit('found', file, elem);
+                });
+            }
+        });
+    });
+}
