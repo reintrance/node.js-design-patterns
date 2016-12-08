c37088b63e0991d99c6a9fd6190e80f21fcf2421
ReInTrance <mklechykau@mklechykau-mac.local>
2016-12-08 21:18:04 +0300

    Revealing pattern and simple module loader started.

--- patterns/module/revealing.js
+++ patterns/module/revealing.js
@@ -1,1 +1,14 @@
-revealing.js
+// Reveal only parts you want to be wisible for end user
+var module = (function () {
+	var privateFoo = function () {
+		// private function logic
+	};
+	var privateVar = [];
+
+	var export = {
+		publicFoo: function () {},
+		publicVar: {}
+	};
+
+	return export;
+})();
--- patterns/module/simple-module-loader.js
+++ patterns/module/simple-module-loader.js
@@ -1,1 +1,9 @@
-simple-module-loader.js
+var fs = require('fs');
+
+function loadModule (filename, module, require) {
+	var wrapSrc = '(function (module, exports, require) {' +
+		fs.readFileSync(filename, 'utf8');
+	+ '})(module, module.exports, require);';
+
+	eval(wrapSrc);
+}
