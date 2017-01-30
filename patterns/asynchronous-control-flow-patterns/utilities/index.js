550580dd10b2dafa58418c465294d7e26117858a
ReInTrance <mklechykau@mklechykau-mac.local>
2017-01-30 17:27:10 +0300

    Implemented getting links from downloaded data

--- patterns/asynchronous-control-flow-patterns/utilities/index.js
+++ patterns/asynchronous-control-flow-patterns/utilities/index.js
@@ -1,5 +1,6 @@
 var url = require('url');
 var path = require('path');
+var getUrls = require('get-urls')
 
 function urlToFileName (urlString) {
     var urlObject = url.parse(urlString);
@@ -8,4 +9,17 @@ function urlToFileName (urlString) {
     return filename;
 }
 
-module.exports.urlToFileName = urlToFileName;
+function getAllLinks (currentUrl, body) {
+    var urls = getUrls(body);
+    var urlObject = url.parse(currentUrl);
+
+    // TODO: investigate set cuz this implementation is ugly.
+    return Array.from(urls).filter((current) => {
+        return current.indexOf(urlObject.host) !== -1;
+    });
+}
+
+module.exports = {
+    urlToFileName,
+    getAllLinks
+};
