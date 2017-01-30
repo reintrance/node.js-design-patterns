068deb42ef19363d9f6788e789b39ce9f3fa7ecd
ReInTrance <mklechykau@mklechykau-mac.local>
2017-01-30 15:13:14 +0300

    Spider revised

--- patterns/asynchronous-control-flow-patterns/spider/spider.js
+++ patterns/asynchronous-control-flow-patterns/spider/spider.js
@@ -4,35 +4,52 @@ var mkdirp = require('mkdirp');
 var path = require('path');
 var utilities = require('./utilities');
 
+function saveFile (filename, contents, callback) {
+    mkdirp(path.dirname(filename), function (err) {
+        if (err) {
+            return callback(err);
+        }
+
+        fs.writeFile(filename, contents, callback);
+    });
+}
+
+function download (url, filename, callback) {
+    console.log('Downloading ' + url);
+
+    request(url, function (err, response, body) {
+        if (err) {
+            return callback(err);
+        } 
+        
+        saveFile(filename, body, function (err) {
+            console.log(`Downloaded and saved: ${url}`);
+
+            if (err) {
+                return callback(err);
+            }
+
+            callback(null, body);
+        });
+        
+    });
+}
+
 function spider (url, callback) {
     var filename = utilities.urlToFileName(url);
 
     fs.exists(filename, function (exists) {
-        if (!exists) {
-            console.log('Downloading ' + url);
+        if (exists) {
+            return callback(null, filename, false);
+        } 
 
-            request(url, function (err, response, body) {
-                if (err) {
-                    callback(err);
-                } else {
-                    mkdirp(path.dirname(filename), function (err) {
-                        if (err) {
-                            callback(err);
-                        } else {
-                            fs.writeFile(filename, body, function (err) {
-                                if (err) {
-                                    callback(err);
-                                } else {
-                                    callback(null, filename, true);
-                                }
-                            });
-                        }
-                    });
-                }
-            });
-        } else {
-            callback(null, filename, false);
-        }
+        download(url, filename, function (err) {
+            if (err) {
+                return callback(err);
+            }
+
+            callback(null, filename, true);
+        })
     });
 }
 
