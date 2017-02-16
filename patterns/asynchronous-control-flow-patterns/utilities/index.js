b9eea910e52ed068c6caac8d6b47f9f39cb6c3b6
ReInTrance <mklechykau@mklechykau-mac.local>
2017-02-16 12:30:09 +0300

    Implemented promisify utility method

--- patterns/asynchronous-control-flow-patterns/utilities/index.js
+++ patterns/asynchronous-control-flow-patterns/utilities/index.js
@@ -19,7 +19,30 @@ function getAllLinks (currentUrl, body) {
     });
 }
 
+function promisify (callbackBasedApi) {
+    return function promisified () {
+        let args = [].slice.call(arguments);
+
+        return new Promise((resolve, reject) => {
+            args.push((err, result) => {
+                if (err) {
+                    return reject(err);
+                }
+
+                if (arguments.length < 2) {
+                    resolve(result);
+                } else {
+                    resolve([].slice.call(arguments, 1));
+                }
+            });
+
+            callbackBasedApi.apply(null, args);
+        });
+    };
+}
+
 module.exports = {
     urlToFileName,
-    getAllLinks
+    getAllLinks,
+    promisify
 };
