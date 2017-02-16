6df56ca9aa62146037473d5818ffe5b1925708e6
ReInTrance <mklechykau@mklechykau-mac.local>
2017-02-16 12:19:05 +0300

    Refactoring of utilities module

--- patterns/asynchronous-control-flow-patterns/utilities/index.js
+++ patterns/asynchronous-control-flow-patterns/utilities/index.js
@@ -1,21 +1,19 @@
-var url = require('url');
-var path = require('path');
-var getUrls = require('get-urls')
+const url = require('url');
+const path = require('path');
+const getUrls = require('get-urls');
 
 function urlToFileName (urlString) {
-    var urlObject = url.parse(urlString);
-    var fileName  = path.basename(urlObject.pathname);
-    var pathToFile = fileName ? urlObject.pathname : path.join(urlObject.pathname, 'index.html')
-    var filename = path.normalize(`${process.cwd()}/downloads/${pathToFile}`);
+    let urlObject = url.parse(urlString);
+    let fileName  = path.basename(urlObject.pathname);
+    let pathToFile = fileName ? urlObject.pathname : path.join(urlObject.pathname, 'index.html');
 
-    return filename;
+    return path.normalize(`${process.cwd()}/downloads/${pathToFile}`);
 }
 
 function getAllLinks (currentUrl, body) {
-    var urls = getUrls(body);
-    var urlObject = url.parse(currentUrl);
-
-    // TODO: investigate set cuz this implementation is ugly.
+    let urls = getUrls(body);
+    let urlObject = url.parse(currentUrl);
+    
     return Array.from(urls).filter((current) => {
         return current.indexOf(urlObject.host) !== -1;
     });
